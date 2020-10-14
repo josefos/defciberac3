1536b45af90fb3934e7b61210340b73c4b15bb39
josefos <jose.teixeira@aluno.faculdadeimpacta.com.br>
2020-10-13 21:40:44 -0300

    ac3

--- t8.py
+++ t8.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify, request
+from flaskjsonify, request
 from math import sqrt
 
 app = Flask(__name__)
@@ -20,19 +20,3 @@ def nao_entre_em_panico():
         for i in rang(2, numero):
             if numero % i == 0:
                 ehprimo =0
-                break
-        if (ehprimo)
-            primos = primos + str(numero) + ","
-            p =+ 1
-            if (p % 10 == 0):
-                primos = primos + "<br>"
-        numero+=1
-
-    return
-
-if __name__ == "__main__"
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
-
-
